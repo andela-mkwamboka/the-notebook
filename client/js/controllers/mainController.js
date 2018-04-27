cdd4616f97362d8b37b78655cf4751a3a35da4f5
andela-mkwamboka <monicah.kwamboka@andela.com>
2018-04-27 16:18:21 +0300

    setup angular

---
+++ client/js/controllers/mainController.js
@@ -0,0 +1,6 @@
+(function() {
+  angular.module('noteApp')
+    .controller('mainController', ($scope) => {
+      $scope.welcome = 'Welcome';
+    });
+}());
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
-  cors = require('cors');
+  cors = require('cors'),
+  path = require('path');
 
 require('dotenv').config();
 
@@ -22,14 +23,13 @@ mongoose.connect(DB_URL, (error) => {
 app.use(cors()); // prevent app from making other calls
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + '/client')));
+
 
 // Routes
 const routes = require('./server/routes/index');
 app.use('/api', routes);
 
-app.get('/', (request, response) => {
-  response.send('Hello world');
-});
 app.listen(process.env.PORT, () => {
   console.log('Application running on port ' + process.env.PORT);
 });
